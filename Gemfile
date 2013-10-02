3fb6ddba03ddea347edadb2c6d31d59dc9ad49d6
Cecep Efendi <cecep.efendi@codecampz.com>
2013-10-02 17:27:56 +0700

    header % footer

--- Gemfile
+++ Gemfile
@@ -30,6 +30,9 @@ gem 'jbuilder', '~> 1.2'
 # Authentication Modul
 gem 'devise'
 
+gem 'bootstrap-sass-rails'
+
+
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,26 @@
+class ArticlesController < ApplicationController
+
+  def index
+  end
+  
+  def edit
+  end
+
+  def show 
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  
+
+end
